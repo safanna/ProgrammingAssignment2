58d10e9be7abea2bae87b25b09f2c8b4c6fc280a
safanna <safonova.anna13@gmail.com>
2017-06-30 11:22:05 +0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+##I assumed that the matrix supplied is always square and invertible (according to instructions)
 
-## Write a short comment describing this function
 
+##The function makeCacheMatrix  creates a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setIM <- function(matrix) m <<- matrix
+  getIM <- function() m
+  list(set = set, get = get,
+       setIM = setIM,
+       getIM = getIM)
 }
-
-
-## Write a short comment describing this function
-
+##The function checks if the inverse has already been calculated, 
+##then retrieve the inverse from the cache. Otherwise calculate it.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getIM()
+  if(!is.null(m)) {
+    message("getting cached inverse matrix")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setIM(m)
+  m
 }
+
+##Check functions using multiplication of matrices
+A<-matrix(c(15,0,-1,6), 2,2)
+q<-makeCacheMatrix(A)
+B<-cacheSolve(q)
+B
+A%*%B
+matrix(1:4,2,2)
+matrix(c(15,0,-1,6), 2,2)
+matrix(c(1,0,0,0,1,0,0,0,1), 3,3)
